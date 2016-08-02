e64ec632d8d4d99cab5f41edb5c87f5755bf88b4
taken1994 <601367440@qq.com>
2016-08-02 16:33:46 +0800

    12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import CircularProgress from 'material-ui/CircularProgress';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 class App extends React.Component {
+  getChildContext() {
+    return {muiTheme: getMuiTheme()};
+  }
+  constructor(props) {
+      super(props);
+      this.state = {
+        user: {},
+        wait:true
+      };
+    }
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/happypeter`)
+    .then((res) => {
+       this.setState({
+         user: res.data,
+         wait:false
+       });
+       console.log(res);
+     });
+  }
   render () {
     return(
-      <h1>脚手架工具</h1>
+      <div>
+        {
+          this.state.wait ? <CircularProgress /> :
+          <div>
+            <img src={this.state.user.avatar_url} />
+            <br/>
+            {this.state.user.name}<br/>
+            {this.state.user.blog}
+          </div>
+        }
+      </div>
     )
   }
 }
+
+App.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import {render} from 'react-dom';
 
 import App from './App.js';
 
+
 render(<App />,document.getElementById('root'));
